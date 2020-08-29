73f56b10f9f584d11a5464bdfdc801fd1f05b28c
sfoutz0205 <68661461+sfoutz0205@users.noreply.github.com>
2020-08-28 22:03:47 -0600

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,16 +71,18 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function(event, ui) {
-    
+    $(this).addClass("dropover")
+    $(".bottom-trash").addClass("bottom-trash-drag");
   },
   deactivate: function(event) {
-    
+    $(this).removeClass("dropover")
+    $(".bottom-trash").removeClass("bottom-trash-drag");
   },
   over: function(event) {
-    
+    $(event.target).addClass("dropover-active")
   },
   out: function(event) {
-    
+    $(event.target).removeClass("dropover-active")
   },
   update: function(event) {
     // array to store the task data in
@@ -121,12 +123,13 @@ $("#trash").droppable({
   tolerance: "touch",
   drop: function(event, ui) {
     ui.draggable.remove();
+    $(".bottom-trash").removeClass("bottom-trash-active");
   },
   over: function(event, ui) {
-    console.log("over");
+    $(".bottom-trash").assClass("bottom-trash-active");
   },
   out: function(event, ui) {
-    console.log("out");
+    
   }
 });
 
@@ -147,7 +150,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -264,4 +267,8 @@ $("#remove-tasks").on("click", function() {
 // load tasks for the first time
 loadTasks();
 
-
+setInterval(function() {
+  $(".card .list-group-item").each(function (el) {
+    auditTask(el);
+  });
+}, 1800000);
